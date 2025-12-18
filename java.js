26272a779b02c402689b2aff6da5ccd1997fe878
Avika359 <darkdevil113456@gmail.com>
2025-12-18 22:47:25 +0530

    Refactor click event handling for sem elements
    Updated event listeners for elements with class 'sem' to include a timeout that removes the 'click' class after 500ms.

--- java.js
+++ java.js
@@ -1,30 +1,50 @@
 for(var i = 0;i<document.querySelectorAll(".sem").length;i++){
     document.querySelectorAll(".sem")[i].addEventListener("click",function(){
-        alert("hello");
         switch (this.innerHTML) {
-            case "em-1":
+            case "Sem-1":
                 document.querySelector(".sem1").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem1").classList.remove("click");
+                },500)
                 break;
-            case "sem-2":
+            case "Sem-2":
                 document.querySelector(".sem2").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem2").classList.remove("click");
+                },500)
                 break;
-            case "sem-3":
+            case "Sem-3":
                 document.querySelector(".sem3").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem3").classList.remove("click");
+                },500)
                 break;
-            case "sem-4":
+            case "Sem-4":
                 document.querySelector(".sem4").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem4").classList.remove("click");
+                },500)
                 break;
-            case "sem-5":
+            case "Sem-5":
                 document.querySelector(".sem5").classList.add("click");
                 break;
-            case "sem-6":
+            case "Sem-6":
                 document.querySelector(".sem6").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem6").classList.remove("click");
+                },500)
                 break;
-            case "sem-7":
+            case "Sem-7":
                 document.querySelector(".sem7").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem7").classList.remove("click");
+                },500)
                 break;
-            case "sem-8":
+            case "Sem-8":
                 document.querySelector(".sem8").classList.add("click");
+                setTimeout(function(){
+                    document.querySelector(".sem8").classList.remove("click");
+                },500)
                 break;
             default:
                 break;
